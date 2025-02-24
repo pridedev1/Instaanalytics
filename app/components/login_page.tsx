541e31c81500893e91a5ea4d79f78362776d0f16
baseplate77 <base8087@gmail.com>
2025-02-24 11:33:32 +0530

    f

--- app/components/login_page.tsx
+++ app/components/login_page.tsx
@@ -21,7 +21,7 @@ const LoginPage = ({ handleLogin }: any) => {
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-    }, 3000); // Change image every 3 seconds
+    }, 3000); // Change image every 3 second
 
     return () => clearInterval(interval);
   }, []);
